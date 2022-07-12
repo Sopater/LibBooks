e6c54105114d9192307b5753113de0fd637674ac
Muhammad Rifki <programmer.mobile1@siklus.id>
2022-07-12 09:16:54 +0700

    Revert "add login and register"
    This reverts commit 138b3f48098035aa300d8867b63e02594d76f902.

--- app.js
+++ app.js
@@ -1,24 +1,20 @@
-require('dotenv').config();
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var expressLayouts = require('express-ejs-layouts')
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const expressLayouts = require('express-ejs-layouts');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const mongoose = require('mongoose');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/auth/login');
+var registerRouter = require('./routes/auth/register');
+var booksRouter = require('./routes/book');
+var pengarangRouter = require('./routes/pengarang');
+var profileRouter = require('./routes/profile');
+var tentangRouter = require('./routes/tentang');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const loginRouter = require('./routes/auth/login');
-const registerRouter = require('./routes/auth/register');
-const booksRouter = require('./routes/book');
-const pengarangRouter = require('./routes/pengarang');
-const profileRouter = require('./routes/profile');
-const tentangRouter = require('./routes/tentang');
-
-const app = express();
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -31,7 +27,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -41,12 +36,6 @@ app.use('/buku', booksRouter);
 app.use('/pengarang', pengarangRouter);
 app.use('/profile', profileRouter);
 app.use('/tentang', tentangRouter);
-
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true }).then((db) => {
-
-  db.connection.on('error', error => console.error(error));
-  db.connection.once('open', () => console.log('koneksi database berhasil'));
-});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -64,4 +53,4 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(process.env.PORT || 3000)
+module.exports = app;
--- routes/auth/login.js
+++ routes/auth/login.js
@@ -1,37 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const bcrypt = require('bcrypt');
-const User = require('../../models/user');
+var express = require('express');
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('auth/login', { layout: 'layouts/layout-auth' });
 });
-
-router.post('/', async (req, res) => {
-    const { userName, password } = req.body;
-
-    await User.findOne({ userName: userName }).then((user) => {
-        if (!user) {
-            return res.status(404).send({
-                message: 'username not found',
-            });
-        }
-        bcrypt.compare(password, user.password, (err, result) => {
-            if (err) return res.status(500).send(err);
-            else if (result) {
-                return res.redirect('/');
-            }
-            return res.render('auth/login', {
-                message: 'Password Salah'
-            });
-        });
-    }).catch((err) => {
-        return res.status(500).send({
-            message: 'Internal Server Error',
-            error: err
-        })
-    });
-})
 
 module.exports = router;
